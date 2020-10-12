5862a07d71b7bac3b804df5f0f7807c6c100a50c
Edouard Berard <edouard.berard@gmail.com>
2020-10-12 01:28:53 +0100

    updated css html & text in sketch2.js

--- sketch2.js
+++ sketch2.js
@@ -1,6 +1,11 @@
-//Credits
+// Credits
 // Most of code from "ml5.js: Pose Regression" by The Coding Train / Daniel Shiffman
 // Modified to adapt variable font
+//
+// All styling mostly happens in this file as I couldn't move it over HTML & CSS
+// Sorry for all the styling tags
+// 
+// 
 
 let video;
 let poseNet;
@@ -9,61 +14,57 @@ let skeleton;
 let brain;
 let fontWeight;
 let button;
-var startB; // the buttons
 var timerValue = 5;
 
 function setup() {
   c = createCanvas(200, 150);
-  c.style('padding','10px');
-  c.style('position','fixed');
-  c.style('bottom','0');
-  c.style('left','0');
-  fontWeight = createSlider(100, 200,100);
+  // Canvas settings to fit only the camera feedback
+  c.style('padding', '10px');
+  c.style('position', 'fixed');
+  c.style('bottom', '0');
+  c.style('left', '0');
+  fontWeight = createSlider(100, 200, 100);
   fontWeight.hide();
-  // demo
+
+  // Variable font feedback
   p = createP('ABCDEFGHIJKLMN<br>OPQRSTUVWXYZ');
-  p.style('position','absolute');
+  p.style('position', 'absolute');
   p.style('top', '25%');
   p.style('margin-left', 'auto');
   p.style('margin-right', 'auto');
   p.style('right', '0');
   p.style('left', '0');
-  p.style('text-align','center');
+  p.style('text-align', 'center');
 
-// bouton
+  // Button 
   button = createButton(" I'm ready! ");
   button.mouseClicked(buttonFunction);
-  button.style('text-decoration','none');
-  button.style('background-color','#B0FE76');
-  //B0FE76 ECFFB0
-  button.style('border','none');
-  button.style('font-size','25px');
-  button.style('position','absolute');
-  button.style('bottom','30%');
-  button.style('left','45%');
-  button.style('text-align','center');
-  button.style('border-radius','45px');
-  button.style('padding','15px');
-  button.style('padding-left','25px');
-  button.style('padding-right','25px');
+  button.style('text-decoration', 'none');
+  button.style('background-color', '#B0FE76');
+  button.style('border', 'none');
+  button.style('font-size', '25px');
+  button.style('position', 'absolute');
+  button.style('bottom', '30%');
+  button.style('left', '45%');
+  button.style('text-align', 'center');
+  button.style('border-radius', '45px');
+  button.style('padding', '15px');
+  button.style('padding-left', '25px');
+  button.style('padding-right', '25px');
 
   //video feedback
   video = createCapture(VIDEO);
   video.hide();
-  //video.size(200, 150);
-  //video.style('padding','10px');
-  //video.style('position','fixed');
-  //video.style('bottom','0');
-  //video.style('left','0');
-  //description text
-  p3 = createP('Having a hard time finding a experimental font? Hit “ready”, take a few step back, do your best pose and generate your unique font.');
-  p3.style('color','black');
+
+  // Description / Instruction styling
+  p3 = createP('Having a hard time finding a experimental font? Hit “ready”, take a few step back (around 2 meters from your webcam), do your best pose and generate your unique font.');
+  p3.style('color', 'black');
   p3.style('font-size', '18px');
   p3.style('font-family', 'MonumentGrotesk');
-  p3.style('position','fixed');
-  p3.style('bottom','0');
-  p3.style('right','0');
-  p3.style('padding','10px');
+  p3.style('position', 'fixed');
+  p3.style('bottom', '0');
+  p3.style('right', '0');
+  p3.style('padding', '10px');
 
   //poseNet set up
   poseNet = ml5.poseNet(video, modelLoaded);
@@ -84,7 +85,7 @@ function setup() {
   brain.load(modelInfo, brainLoaded);
 }
 
-
+// PoseNet
 
 function brainLoaded() {
   console.log('pose predicting ready!');
@@ -107,12 +108,12 @@ function predictWeight() {
 }
 
 function gotResult(error, results) {
-  
+
   console.log(results);
   let weight = results[0].value;
   fontWeight.value(weight);
   predictWeight();
-  
+
 }
 
 function gotPoses(poses) {
@@ -123,87 +124,67 @@ function gotPoses(poses) {
   }
 }
 
-
 function modelLoaded() {
   console.log('poseNet ready');
 }
 
-function buttonFunction(){
+function buttonFunction() {
   timerCountdown();
-  setTimeout(fontDownload,6000);
+  setTimeout(fontDownload, 6000);
 }
 
+// Countdown before getting the font from pose
+
 function timerCountdown() {
-  setInterval(function() {
+  setInterval(function () {
     if (timerValue > 0) {
       timerValue--;
     }
   }, 1000);
 }
 
-function fontDownload() {
-    linkNum = map(fontWeight.value(),100,200,100,200);
-    button.hide();
-    l = createButton('Download your unique font!');
-    l.mousePressed(downloadLink)
-    l.style('background-color','#B0FE76');
-    l.style('border','none');
-    l.style('font-size','25px');
-    l.style('position','absolute');
-    l.style('bottom','30%');
-    l.style('left','39%');
-    l.style('text-align','center');
-    l.style('border-radius','45px');
-    l.style('padding','16px');
-    l.style('padding-left','25px');
-    l.style('padding-right','25px');
-  }
+// Button function to get the right font from pose
 
-  function downloadLink() {
-    window.open('https://1998edo.shortcm.li/ssvar'+linkNum);
-  }
+function fontDownload() {
+  linkNum = map(fontWeight.value(), 100, 200, 100, 200);
+  button.hide();
+  l = createButton('Download your unique font!');
+  l.mousePressed(downloadLink)
+  l.style('background-color', '#B0FE76');
+  l.style('border', 'none');
+  l.style('font-size', '25px');
+  l.style('position', 'absolute');
+  l.style('bottom', '30%');
+  l.style('left', '39%');
+  l.style('text-align', 'center');
+  l.style('border-radius', '45px');
+  l.style('padding', '16px');
+  l.style('padding-left', '25px');
+  l.style('padding-right', '25px');
+}
+
+function downloadLink() {
+  window.open('https://1998edo.shortcm.li/ssvar' + linkNum);
+}
+
+// Setting up the variable weight value of the font, video feedback & the countdown
 
 function draw() {
- /* push();
-  translate(video.width, 0);
-  scale(-1, 1);
-  image(video, 0, 0, video.width, video.height);
-
-  if (pose) {
-    for (let i = 0; i < skeleton.length; i++) {
-      let a = skeleton[i][0];
-      let b = skeleton[i][1];
-      strokeWeight(2);
-      stroke(0);
-
-      line(a.position.x, a.position.y, b.position.x, b.position.y);
-    }
-    for (let i = 0; i < pose.keypoints.length; i++) {
-      let x = pose.keypoints[i].position.x;
-      let y = pose.keypoints[i].position.y;
-      fill(0);
-      stroke(255);
-      ellipse(x, y, 16, 16);
-    }
-  }
-  pop();*/
   image(video, 0, 0, 200, 150);
-  //image.style(
-    if (timerValue > 5) {
-      text("", width / 2, height / 2);
-      background('rgba(176,254,118, 0)');
-    }  if  (timerValue < 5){
-      background('rgba(176,254,118, 1)');
-      text(timerValue, width / 2, height / 2);
-      textSize(100);
-      textAlign(CENTER,CENTER);
-    }  if (timerValue ==0 ) {
-      image(video, 0, 0, 200, 150);
-    }
+  if (timerValue > 5) {
+    text("", width / 2, height / 2);
+    background('rgba(176,254,118, 0)');
+  } if (timerValue < 5) {
+    background('rgba(176,254,118, 1)');
+    text(timerValue, width / 2, height / 2);
+    textSize(100);
+    textAlign(CENTER, CENTER);
+  } if (timerValue == 0) {
+    image(video, 0, 0, 200, 150);
+  }
   let pfat = fontWeight.value();
-  p.style('font-weight' ,pfat);
- //print(pfat);
-
+  p.style('font-weight', pfat);
+  //print(pfat);
 }
 
 
